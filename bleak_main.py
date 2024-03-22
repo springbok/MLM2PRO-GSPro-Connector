0c2bd126f76418078ae441710447bcc93f979a07
Etienne van Tonder <etiennevt@gmail.com>
2024-03-23 07:54:31 +1100

    BT

--- bleak_main.py
+++ bleak_main.py
@@ -8,11 +8,15 @@ from src.mlm2pro_bluetooth.scanner import MLM2PROScanner
 device = None
 
 async def main():
-    for dev in await MLM2PROScanner.discover():
-        print(f"Device: {dev}")
+    scanner = MLM2PROScanner()
+    loop = asyncio.get_event_loop()
+    await scanner.run(loop)
+
+    if scanner.device is not None:
+        print(f"Device: {scanner.device}")
         try:
-            print(f'{dev}')
-            async with MLM2PROClient(dev) as mlm2pro_client:
+            print(f'{scanner.device}')
+            async with MLM2PROClient(scanner.device) as mlm2pro_client:
                 print(mlm2pro_client.is_connected)
                 async with MLM2PROAPI(mlm2pro_client) as api:
                     result = await api.auth()
@@ -21,6 +25,9 @@ async def main():
                 #print(f'firmware version: {await api.read_firmware_version()}')
         except Exception as e:
             print(f'Error: {format(e)}, {traceback.format_exc()}')
+
+    else:
+        print('No device found')
 
 if __name__ == '__main__':
     asyncio.run(main())
--- src/mlm2pro_bluetooth/scanner.py
+++ src/mlm2pro_bluetooth/scanner.py
@@ -1,12 +1,32 @@
-from bleak import BLEDevice
-from bleak import BleakScanner
+import asyncio
+from bleak import BleakScanner, BLEDevice, AdvertisementData
 
 
 class MLM2PROScanner:
+    timeout_seconds = 20
     MLM2PRO_NAME_PREFIX = "MLM2-"
     BLUEZ_NAME_PREFIX = "BlueZ"
 
-    @classmethod
-    async def discover(cls, timeout: float = 5) -> list[BLEDevice]:
-        devices = await BleakScanner.discover(timeout=timeout)  # type: ignore
-        return [dev for dev in devices if dev.name and (dev.name.startswith(MLM2PROScanner.MLM2PRO_NAME_PREFIX) or dev.name.startswith(MLM2PROScanner.BLUEZ_NAME_PREFIX))]
+    def __init__(self):
+        self._scanner = BleakScanner(detection_callback=self.__detection_callback)
+        self.scanning = asyncio.Event()
+        self.device = None
+
+    def __detection_callback(self, device: BLEDevice, advertisement_data: AdvertisementData):
+        print(f'detection_callback: {device}')
+        if device.name and (device.name.startswith(MLM2PROScanner.MLM2PRO_NAME_PREFIX) or device.name.startswith(MLM2PROScanner.BLUEZ_NAME_PREFIX)):
+            print(f"{device.name} {device.address} {advertisement_data}")
+            print(f"Device found: {device.name}")
+            self.device = device
+            self.scanning.clear()
+
+    async def run(self, loop):
+        await self._scanner.start()
+        self.scanning.set()
+        end_time = loop.time() + MLM2PROScanner.timeout_seconds
+        while self.scanning.is_set():
+            if loop.time() > end_time:
+                self.scanning.clear()
+                print('Scan has timed out, no device found')
+            await asyncio.sleep(0.1)
+        await self._scanner.stop()
