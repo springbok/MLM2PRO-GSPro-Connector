1b0f46881164ffbb8cbfeb010cccc5f5a5ef6395
Etienne van Tonder <etiennevt@gmail.com>
2024-04-08 16:24:01 +1000

    Update main.py

--- src/main.py
+++ src/main.py
@@ -1,5 +1,5 @@
 import sys
-from PySide6 import QtWidgets
+from PySide6 import QtWidgets, QtAsyncio
 from src.MainWindow import MainWindow
 from src.get_mutex import GetMutex
 
@@ -15,4 +15,4 @@ def main():
     window = MainWindow(app)
     window.show()
 
-    app.exec()
+    QtAsyncio.run()
