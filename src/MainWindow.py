d17dec748559c5c4590bc3ca0026b547427186e7
Etienne van Tonder <etiennevt@gmail.com>
2024-04-14 15:03:12 +1000

    Change RSSI levels

--- src/MainWindow.py
+++ src/MainWindow.py
@@ -31,7 +31,7 @@ class LogTableCols:
 
 
 class MainWindow(QMainWindow, Ui_MainWindow):
-    version = 'V1.03.11'
+    version = 'V1.03.12'
     app_name = 'MLM2PRO-GSPro-Connector'
     good_shot_color = '#62ff00'
     good_putt_color = '#fbff00'
